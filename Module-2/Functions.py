7b81253f6f42d724574f9e7812bafd8338706e84
Damasus Okeke <gwill2be@gmail.com>
2026-06-22 16:52:53 +0200

    Added code's Big O complexity evaluation

--- Module-2/Functions.py
+++ Module-2/Functions.py
@@ -3,3 +3,7 @@ def is_even(number):
   return number % 2 == 0
 result = is_even(3)
 print(result)
+
+# Complexity = O(1) (constant number of operations): This is because the function takes only one items (element) and checks if it's 
+# an even number. That will always perform the same number of operations no matter how big or small the element is
+# as what matters is the number of operations to be performed, which will always be the same. 
