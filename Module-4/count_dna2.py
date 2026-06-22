76fef0d5fff5da0183e625b4ac9fcba930847ca4
Damasus Okeke <gwill2be@gmail.com>
2026-06-22 16:50:02 +0200

    Added another (better) solution to count_dna problem

---
+++ Module-4/count_dna2.py
@@ -0,0 +1,7 @@
+def count_nucleotide(dna: str) -> tuple[int, int, int, int]:
+  """Count A, C, G, T occurances in a DNA string. Returns counts in that order."""
+  return dna.count('A'), dna.count('C'), dna.count('G'), dna.count('T')
+if __name__ == "__main__":  # means: only run this code if this file was executed directly
+  dna_nucleotide = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
+  a, c, g, t = count_nucleotide(dna_nucleotide)
+  print(f"{a} {c} {g} {t}")
