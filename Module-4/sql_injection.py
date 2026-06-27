c36066b461574d8ac3e2b5b75e5dc3f1083cb80d
Damasus Okeke <gwill2be@gmail.com>
2026-06-27 15:36:25 +0200

    I learned how to identify and handle SQL injection vulnerability.

--- Module-4/sql_injection.py
+++ Module-4/sql_injection.py
@@ -1,9 +1,9 @@
 # SQL injection vulnerable
 def search_products(search_term):
-  query = f"SELECT * FROM products WHERE name LIKE '%{search_term}%'"
+  query = f"SELECT * FROM products WHERE name LIKE '%{search_term}%'"    # pass user input as an executable code
   return query
 
 # Safe version
 def search_product(search_term, cursor):
-  query = "SELECT * FROM products WHERE name LIKE ?"
-  cursor.execute(query, (search_term,))
+  query = "SELECT * FROM products WHERE name LIKE ?"    # treats user input (search_term) as a data. "?" is a placeholder. It separates the query structure from the user input.
+  cursor.execute(query, (search_term,))    # "," after search_term ensures that the input is a tuple.
