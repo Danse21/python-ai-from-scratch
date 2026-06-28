2e1f9e07ee377df7c8b4a6cb886deb041577504d
Damasus Okeke <gwill2be@gmail.com>
2026-06-28 19:54:24 +0200

    Improved execution time of DNA count (count_dna2.py file) from time complexity of O(4n) to O(n), that is, 4 times faster.

--- Module_4/count_dna2.py
+++ Module_4/count_dna2.py
@@ -1,6 +1,9 @@
+
+from collections import Counter
 def count_nucleotide(dna: str) -> tuple[int, int, int, int]:
   """Count A, C, G, T occurances in a DNA string. Returns counts in that order."""
-  return dna.count('A'), dna.count('C'), dna.count('G'), dna.count('T')
+  counts = Counter(dna)
+  return counts['A'], counts['C'], counts['G'], counts['T']
 if __name__ == "__main__":  # means: only run this block of code if this file was executed directly
   dna_nucleotide = "AGCTTTTCATTCTGACTGCAACGGGCAATATGTCTCTGTGTGGATTAAAAAAAGAGTGTCTGATAGCAGC"
   a, c, g, t = count_nucleotide(dna_nucleotide)
