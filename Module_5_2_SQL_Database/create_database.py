b1272bf17123455a025684868883e6dc9c1e406b
Damasus Okeke <gwill2be@gmail.com>
2026-07-06 20:58:35 +0200

    Learned to write INNER JOIN and LEFT JOIN queries in SQL to join datasets that do not perfectly overlap.

--- Module_5_2_SQL_Database/create_database.py
+++ Module_5_2_SQL_Database/create_database.py
@@ -40,16 +40,69 @@ conn.commit()
 
 # Query and print all rows
 df = pd.read_sql_query("SELECT * FROM sequences", conn)
-print(df)
+# print(df)
 
-# Write three separate queries:
+# Övning 2: Write three separate queries:
 # All sequences from a specific organism
 df2 = pd.read_sql_query("SELECT * FROM sequences WHERE organism = ?", conn, params=("H. sapiens",))
-print(df2)
+# print(df2)
 # All sequences with gc_content above 50%, ordered highest to lowest
 df3 = pd.read_sql_query("SELECT * FROM sequences WHERE gc_content > 50 ORDER BY gc_content DESC", conn)
-print(df3)
+# print(df3)
 # The average gc_content across all sequences
 df4 = pd.read_sql_query("SELECT AVG(gc_content) AS avg_gc_content FROM sequences", conn)
-print(df4)
+# print(df4)
 
+
+# Övning 3:
+# Create a second table organisms (columns: id, name, kingdom, genome_size).
+# Insert matching organism data. Write a JOIN query that returns sample_id,
+# gc_content, kingdom, and genome_size for all sequences. Then write a LEFT JOIN version
+# and explain the difference in output.
+
+cursor.execute("DROP TABLE IF EXISTS organisms")
+cursor.execute("""
+      CREATE TABLE IF NOT EXISTS organisms (
+              id            INTEGER PRIMARY KEY AUTOINCREMENT,
+              name          TEXT NOT NULL,
+              kingdom       TEXT NOT NULL,
+              genome_size   INTEGER NOT NULL
+      )
+""")
+conn.commit()
+
+data_organism = [
+  ("E. coli", "Bacteria", 4600000),
+  ("S. cerevisiae", "Fungi", 12100000),
+  ("A. thaliana", "Plantae", 135000000),
+  ("D. melanogaster", "Animalia", 143700000),
+  ("H. sapiens", "Animalia", 3200000000)
+]
+
+# Insert the data above into the organisms table
+cursor.executemany("""
+      INSERT INTO organisms (name, kingdom, genome_size)
+      VALUES (?, ?, ?)
+""", data_organism)
+conn.commit()
+
+# Write (INNER) JOIN query that returns sample_id, gc_content, kingdom, and genome_size for all sequences
+df5 = pd.read_sql_query("""
+                        SELECT s.sample_id, s.gc_content, o.kingdom, o.genome_size
+                        FROM sequences s
+                        JOIN organisms o ON s.organism = o.name
+                        """, conn)
+print(df5)
+
+# write a LEFT JOIN version and explain the difference in output.
+df6 = pd.read_sql_query("""
+                        SELECT s.sample_id, o.genome_size
+                        FROM sequences s
+                        LEFT JOIN organisms o ON s.organism = o.name
+""", conn)
+print(df6)
+
+# The difference (INNER) JOIN query and LEFT JOIN query is that the JOIN query returns only the rows from both
+# tables that have matches (value under sequences.organism == value under organisms.name)
+# wheres the LEFT JOIN query returns all the rows from the left table and only rows in the right table
+# that match the left table rows, with NaN for rows on the right table without matches.
